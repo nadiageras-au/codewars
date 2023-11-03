414316c13aee36e03aca7aa1b641c70e541c8501
nadia <nadiageras.au@gmail.com>
2023-11-03 23:41:12 +1000

    codewars - credit card mask

---
+++ 7kyu/credit-card-mask.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples (input --> output):
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+//SOLUTION
+
+// return masked string
+function maskify(cc) {
+  let ccArr = cc.split('');
+  for (let i = 0; i < ccArr.length - 4; i++) {
+    ccArr[i] = '#';
+  }
+  let ccMasked = ccArr.join('');
+  return ccMasked;
+}
+
+console.log(maskify(123456789098));
